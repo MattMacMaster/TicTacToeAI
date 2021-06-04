c144b063bf9790471bf43a0c2bb725914428b736
Mort09 <matthew.macmaster@routeique.com>
2021-06-04 12:44:59 -0600

    Working ai, now to make less of an ass and include depth for decisions

--- main.js
+++ main.js
@@ -9,28 +9,65 @@ const Player = ((val) => {
 });
 
 const gameBoard = (() => {
-    const board = ["O", "X", "O", "X", "X", "O", "", "", ""];
+    let board = [
+        ['X', 'O', ''],
+        ['X', 'O', ''],
+        ['', '', '']
+    ];
     const setField = (index, val) => {
-        if (index > board.length) return;
-        board[index] = val;
+        if (index > 8 || index < 0) return;
+
+        if (index < 3) {
+            board[0][index] = val;
+        }
+        else if (index > 2 && index < 6) {
+            board[1][index % 3] = val;
+        }
+        else {
+            board[2][index % 6] = val;
+        }
     }
     const getField = (index) => {
-        if (index > board.length) return;
-        return board[index];
+        if (index > 8 || index < 0) return;
+        if (index < 3) {
+            return board[0][index];
+        }
+        else if (index > 2 && index < 6) {
+            return board[1][index % 3];
+        }
+        else {
+            return board[2][index % 6];
+        }
+
     };
     const reset = () => {
-        for (let i = 0; i < board.length; i++) {
-            board[i] = "";
+        for (let i = 0; i < 3; i++) {
+            for (let x = 0; x < 3; x++) {
+                board[i][x] = '';
+            }
         }
     };
     const returnBoard = () => { return board };
-    const clearIndex = (index) => { board[index] = "" };
+    const clearIndex = (index) => {
+        if (index < 3) {
+            board[0][index] = '';
+        }
+        else if (index > 2 && index < 6) {
+            board[1][index % 3] = '';
+        }
+        else {
+            board[2][index % 6] = '';
+        }
+    };
     const availSpots = () => {
         var indices = [];
-        for (let i = 0; i < board.length; i++) {
-            if (board[i] == "") {
-                indices.push(i);
+        for (let i = 0; i < 3; i++) {
+            for (let x = 0; x < 3; x++) {
+                if (board[i][x] == "") {
+                    indices.push([i, x]);
+                }
             }
+
         }
         return indices;
     };
@@ -80,59 +117,33 @@ const gameController = (() => {
     let round = 1;
     let isOver = false;
 
-    const playRound = async (index) => {
+    const playRound = (index) => {
         if (!isOver) {
             gameBoard.setField(index, getCurrentPlayerVal());
-            if (!checkWinner()) {
-                round = round + 1;
-                displayController.setMessageElement(`Player ${getCurrentPlayerVal()}'s Turn`);
 
-            }
-            if (round == 10) {
+            let result = checkWinner();
+            if (result == "tie") {
+                console.log();
                 displayController.setResultMessage('Draw');
-            }
+                isOver = true;
+
+            };
+            round = round + 1;
+            if (!result) {
+                displayController.setMessageElement(`Player ${getCurrentPlayerVal()}'s Turn`);
+            };
             if (round % 2 == 0) {
-
-                playRound(computerController.best_move());
-            }
-
+                playRound(bestMove(gameBoard.returnBoard()));
+            };
         }
+
+
+
     };
     const getCurrentPlayerVal = () => {
         return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
     };
-    const checkWinner = () => {
-        const winOptions = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
-        ]
-        let winCon = false;
-        winOptions.forEach(winSet => {
-            if (
-                gameBoard.getField(winSet[0]) == gameBoard.getField(winSet[1])
-                &&
-                gameBoard.getField(winSet[2]) == gameBoard.getField(winSet[0])
-                &&
-                gameBoard.getField(winSet[2]) == gameBoard.getField(winSet[1])
-                &&
-                gameBoard.getField(winSet[2]) != ""
-            ) {
-                winCon = true;
-            }
-        });
-        if (winCon) {
-            displayController.setResultMessage(getCurrentPlayerVal());
-            isOver = true;
-            return true;
-        }
-        return false;
-    };
+
     const reset = () => {
         round = 1;
         isOver = false;
@@ -142,116 +153,41 @@ const gameController = (() => {
 })();
 
 
-const computerController = (() => {
-    //Use this function insetead of the best move function for simply random choices
-    const compRandChoice = () => {
-        var choice = Math.floor((Math.random() * gameBoard.availSpots().length));
-        return gameBoard.availSpots()[choice];
-    };
-    const checkWinnerAI = (board) => {
-        const winOptions = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
-        ];
-        winOptions.forEach(winSet => {
-            if (board[winSet[0]] == board[winSet[1]] &&
-                board[winSet[2]] == board[winSet[0]] &&
-                board[winSet[2]] == board[winSet[1]] &&
-                board[winSet[2]] != ""
-            ) {
-                if (board[winSet[0]] == "O") {
-                    //1 Means AI Wins
-                    return 1;
-                }
-                else {
-                    //2 Means Player Wins
-                    return 2;
-                }
-            };
-        });
-        //3 Means a Tie
-        //Null means game goes on with more choices to be made
-        if (board.includes("")) { return null } else return 3;
-    };
+function checkWinner() {
+    let winCon = null;
 
-    const availableIndexes = (board) => {
-        var indexes = [];
-        for (let i = 0; i < board.length; i++) {
-            if (board[i] == "") {
-                indexes.push(i);
-            }
+    const winOptions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    winOptions.forEach(winSet => {
+        if (
+            gameBoard.getField(winSet[0]) == gameBoard.getField(winSet[1])
+            &&
+            gameBoard.getField(winSet[2]) == gameBoard.getField(winSet[0])
+            &&
+            gameBoard.getField(winSet[2]) == gameBoard.getField(winSet[1])
+            &&
+            gameBoard.getField(winSet[2]) != ""
+        ) {
+            winCon = gameController.getCurrentPlayerVal();
         }
-        return indexes;
-    };
-
-    const best_move = () => {
-        let bestScore = -Infinity;
-        let bestMove;
-        let tempBoard = Array.from(gameBoard.returnBoard());
-
-        gameBoard.availSpots().forEach((index) => {
-            tempBoard[index] = "O";
-            let score = minimax(false, tempBoard, 0);
-            tempBoard[index] = "";
-            console.log(`Score:${score}`);
-
-            if (score > bestScore) {
-                bestScore = score;
-                bestMove = index;
-            };
-        });
-        return bestMove;
-    };
-
-
-    const minimax = (isMaxTurn, board, depth) => {
-        let currBoard = board;
-        const result = checkWinnerAI(currBoard);
-        console.log(currBoard);
-
-        if (result == 2) {
-            return -1;
-        }
-        else if (result == 1) {
-            return 1;
-        }
-        else if (result == 3) {
-
-            return 0;
-        }
-        else {
-            if (isMaxTurn) {
-                let bestScore = -Infinity;
-                availableIndexes(currBoard).forEach((index) => {
-                    currBoard[index] = "O";
-                    let score = minimax(!isMaxTurn, currBoard, depth + 1);
-                    currBoard[index] = "";
-                    bestScore = Math.max(score, bestScore);
-                });
-                return bestScore;
-            } else {
-                let bestScore = Infinity;
-                availableIndexes(currBoard).forEach((index) => {
-                    currBoard[index] = "X";
-                    let score = minimax(!isMaxTurn, currBoard, depth + 1);
-                    currBoard[index] = "";
-                    bestScore = Math.min(score, bestScore);
-                });
-                return bestScore;
-            };
-
-        }
-    };
-
-    return { best_move, compRandChoice }
-
-})();
-
-
+    });
+    if (winCon == null && gameBoard.availSpots().length == 0) {
+        return "tie";
+    }
+    //No winner Yet if null, or return winner X or O
+    if (winCon != null) {
+        displayController.setResultMessage(winCon);
+        return winCon;
+    }
+    return winCon;
+};
 displayController.updateGameboard();
